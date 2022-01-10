c3b858443a09da3947794c1dab86dffca982251d
emojapaula <vulpaula@gmail.com>
2022-01-10 20:44:13 +0100

    Column number display

--- components/Board.tsx
+++ components/Board.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { theme } from '../constants/Theme';
 import { useGameData } from '../context/gameContext';
 import MonsterCard from './MonsterCard';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 interface IBoard {
   first?: string[];
@@ -12,7 +13,10 @@ interface IBoard {
 
 const Container = styled(View)`
   width: 95%;
-  margin: 0 auto
+  margin: 0 auto;
+`;
+
+const Game = styled(View)`
   background-color: ${theme.palette.grey};
 `;
 
@@ -22,25 +26,45 @@ const Row = styled(View)`
   justify-content: space-between;
 `;
 
-export const Board: FC<IBoard> = ({ solution }) => {
-  const renderItem = ({ item }: { item: string }) => <MonsterCard monster={item} key={Math.random()} />;
-  const getRandom = (min: number, max: number) => {
-    return Math.floor(Math.random() * (max - min) + min);
-  };
+const ColumnNumberContainer = styled(View)`
+  width: ${wp('11.9%')}px;
+  height: ${hp('6.25%')}px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
+const ColumnNumber = styled(Text)`
+  font-family: ${theme.fonts.openSemi};
+  font-size: ${hp('2%')}px;
+  color: ${theme.palette.primary};
+`;
+
+export const Board: FC<IBoard> = ({ solution }) => {
+  const columns = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+  const renderItem = ({ item }: { item: string }) => <MonsterCard monster={item} key={Math.random()} />;
+  const renderColumn = ({ item }: { item: number }) => (
+    <ColumnNumberContainer>
+      <ColumnNumber>{item}</ColumnNumber>
+    </ColumnNumberContainer>
+  );
   const { board } = useGameData();
   const first = board;
 
   return (
     <Container>
-      <Row>{first.slice(0, 8).map((item: string) => renderItem({ item }))}</Row>
-      <Row>{first.slice(8, 16).map((item: string) => renderItem({ item }))}</Row>
-      <Row>{first.slice(16, 24).map((item: string) => renderItem({ item }))}</Row>
-      <Row>{first.slice(24, 32).map((item: string) => renderItem({ item }))}</Row>
-      <Row>{first.slice(32, 40).map((item: string) => renderItem({ item }))}</Row>
-      <Row>{first.slice(40, 48).map((item: string) => renderItem({ item }))}</Row>
-      <Row>{first.slice(48, 56).map((item: string) => renderItem({ item }))}</Row>
-      <Row>{first.slice(56, 64).map((item: string) => renderItem({ item }))}</Row>
+      <Row>{columns.map((item: string) => renderColumn({ item }))}</Row>
+      <Game>
+        <Row>{first.slice(0, 8).map((item: string) => renderItem({ item }))}</Row>
+        <Row>{first.slice(8, 16).map((item: string) => renderItem({ item }))}</Row>
+        <Row>{first.slice(16, 24).map((item: string) => renderItem({ item }))}</Row>
+        <Row>{first.slice(24, 32).map((item: string) => renderItem({ item }))}</Row>
+        <Row>{first.slice(32, 40).map((item: string) => renderItem({ item }))}</Row>
+        <Row>{first.slice(40, 48).map((item: string) => renderItem({ item }))}</Row>
+        <Row>{first.slice(48, 56).map((item: string) => renderItem({ item }))}</Row>
+        <Row>{first.slice(56, 64).map((item: string) => renderItem({ item }))}</Row>
+      </Game>
     </Container>
   );
 };
